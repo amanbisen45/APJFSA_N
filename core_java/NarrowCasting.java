111af48e36858d04e61b9d2203d5a40849b141bb
Aman Bisen <128792404+amanbisen45@users.noreply.github.com>
2024-10-24 17:54:15 +0530

    Add files via upload

---
+++ core_java/NarrowCasting.java
@@ -0,0 +1,24 @@
+package core_java;
+
+public class NarrowCasting {
+
+	public static void main(String[] args) {
+
+		// bigger  data type
+		double doubleA = 10.8;
+
+		// Narrow casting double to integer  
+		int intA = (int) doubleA;
+
+		//printing double value
+		System.out.println(" double value: " + doubleA);
+
+		//printing integer value
+		System.out.println(" Narrowed integer value: " + intA);
+	}
+}
+
+
+/*  double value: 10.8
+    Narrowed integer value: 10
+ */
